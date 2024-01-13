cd079190d59f5f8b9ce671c49e013a3a8f4401c3
User <User@WIN-8BH8LKBRQNO>
2024-01-13 18:52:58 +0500

    amalyot1

--- Amaliy topshiriq1/Program.cs
+++ Amaliy topshiriq1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("N sonini kiriting: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int birliklar_xonasi = N % 10;
+int onliklar_xonasi = (N / 10) % 10;
+int yuzliklar_xonasi = (N / 100) % 10;
+int mingliklar_xonasi = N / 1000;
+
+Console.WriteLine(mingliklar_xonasi * yuzliklar_xonasi * onliklar_xonasi * birliklar_xonasi);
